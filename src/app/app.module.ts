5c033d04867339bcb42f3e8ccbc4f5bc4fa077b9
alatif2 <aleem.latif@macquarie.com>
2021-03-04 19:38:24 +1100

    -- NGRX counter app integration tweaks

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { CounterComponent } from './counter/counter.component';
+import { CounterComponent } from './components/counter/counter.component';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
+
+import { StoreModule } from '@ngrx/store';
+import { counterReducer } from './components/counter/counter.reducer';
 
 @NgModule({
   declarations: [
@@ -16,7 +19,8 @@ import { environment } from '../environments/environment';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    StoreModule.forRoot({ count: counterReducer })
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/counter/counter.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Store } from '@ngrx/store';
+
+import { increment, decrement, reset } from './counter.actions';
+
+@Component({
+  selector: 'app-counter',
+  templateUrl: './counter.component.html',
+  styleUrls: ['./counter.component.scss']
+})
+export class CounterComponent implements OnInit {
+
+  count$: Observable<number>;
+
+  constructor(private store: Store<{ count: number }>) {
+    this.count$ = store.select('count');
+  }
+
+  increment(): void {
+    this.store.dispatch(increment());
+  }
+
+  decrement(): void {
+    this.store.dispatch(decrement());
+  }
+
+  reset(): void {
+    this.store.dispatch(reset());
+  }
+
+  ngOnInit(): void {
+  }
+
+}
